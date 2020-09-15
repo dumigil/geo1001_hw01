e51a4db1e7a63c7c98cff101efbc6def99f6e2fa
Michiel de Jong <m.d.dejong-1@student.tudelft.nl>
2020-09-15 22:39:36 +0200

    Added functions to plot temperature data

--- geo1001_hw01.py
+++ geo1001_hw01.py
@@ -5,12 +5,47 @@
 import pandas as pd
 import csv
 import xlrd
+import matplotlib.pyplot as plt
+import numpy as np
 from read_data import read_data
 
 
 A,B,C,D,E = read_data()
-print(A.mean())
-print(B.mean())
-print(C.mean())
-print(D.mean())
-print(E.mean())
+
+def plot_temps_hist_5():
+    temp_A = A.Temperature
+    temp_B = B.Temperature
+    temp_C = C.Temperature
+    temp_D = D.Temperature
+    temp_E = E.Temperature
+
+    fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1, 5)
+    fig.suptitle('Temperature histograms with 5 bins')
+    ax1.hist(x=temp_A, bins=5, color='gold', label='Sensor A' )
+    ax2.hist(x=temp_B, bins=5, color='greenyellow', label='Sensor B' )
+    ax3.hist(x=temp_C, bins=5, color='aqua', label='Sensor C' )
+    ax4.hist(x=temp_D, bins=5, color='royalblue', label='Sensor D' )
+    ax5.hist(x=temp_E, bins=5, color='hotpink',label='Sensor E' )
+    plt.show()
+
+def plot_temps_hist_50():
+    temp_A = A.Temperature
+    temp_B = B.Temperature
+    temp_C = C.Temperature
+    temp_D = D.Temperature
+    temp_E = E.Temperature
+
+    fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1, 5)
+    fig.suptitle('Temperature histograms with 50 bins')
+    ax1.hist(x=temp_A, bins=50, color='gold', label='Sensor A' )
+    ax2.hist(x=temp_B, bins=50, color='greenyellow', label='Sensor B' )
+    ax3.hist(x=temp_C, bins=50, color='aqua', label='Sensor C' )
+    ax4.hist(x=temp_D, bins=50, color='royalblue', label='Sensor D' )
+    ax5.hist(x=temp_E, bins=50, color='hotpink',label='Sensor E' )
+    plt.show()
+
+
+
+
+plot_temps_hist_5()
+plot_temps_hist_50()
