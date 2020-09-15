d94719a4faaf6af20c7149298aa67acb35e1a237
Michiel de Jong <m.d.dejong-1@student.tudelft.nl>
2020-09-15 15:48:31 +0200

    Clean up some files, add everything correctly

---
+++ README.md
@@ -0,0 +1,2 @@
+# geo1001_hw01
+Assignment 1 for GEO1001
--- geo1001_hw01.py
+++ geo1001_hw01.py
@@ -2,3 +2,10 @@
 #--Michiel de Jong
 #--4376978
 
+import pandas as pd
+import csv
+import xlrd
+from read_data import read_data
+
+def mean_data_A():
+    print('Mean Wind Speed = ' + str()))
