f664b027a2c32cf35ab584d3e249fefff49fd672
Michiel de Jong <m.d.dejong-1@student.tudelft.nl>
2020-09-16 14:02:46 +0200

    Added boxplot functions

--- geo1001_hw01.py
+++ geo1001_hw01.py
@@ -51,32 +51,125 @@ def frequency_polygon():
     temp_D = D.Temperature
     temp_E = E.Temperature
     fig = plt.figure(figsize=(16,8))
-    plt.hist(temp_A, bins=10, alpha=0.5, label='Sensor A', rwidth=0.85)
+    fig.suptitle('Frequency Polygons for 5 sensors')
+    #Frequency polygon Sensor A
     [freq_A, bins]=np.histogram(temp_A, bins = 10) 
-    cdf_temp_A = (freq_A)  
+    cdf_temp_A = np.cumsum(freq_A)  
     plt.plot(bins[:-1], cdf_temp_A)
-    plt.hist(temp_B, bins=10, alpha=0.5, label='Sensor B', rwidth=0.85)
+    #Frequency polygon Sensor B
     [freq_B, bins]=np.histogram(temp_B, bins = 10) 
     cdf_temp_B = np.cumsum(freq_B)  
     plt.plot(bins[:-1], cdf_temp_B)
-    plt.hist(temp_C, bins=10, alpha=0.5, label='Sensor C', rwidth=0.85)
+    #Frequency polygon Sensor C
     [freq_C, bins]=np.histogram(temp_C, bins = 10) 
     cdf_temp_C = np.cumsum(freq_C)  
     plt.plot(bins[:-1], cdf_temp_C)
-    plt.hist(temp_D, bins=10, alpha=0.5, label='Sensor D', rwidth=0.85)
+    #Frequency polygon Sensor D
     [freq_D, bins]=np.histogram(temp_D, bins = 10) 
     cdf_temp_D = np.cumsum(freq_D)  
     plt.plot(bins[:-1], cdf_temp_D)
-    plt.hist(temp_E, bins=10, alpha=0.5, label='Sensor E', rwidth=0.85)
+    #Frequency polygon Sensor E
     [freq_E, bins]=np.histogram(temp_E, bins = 10) 
     cdf_temp_E = np.cumsum(freq_E)  
     plt.plot(bins[:-1], cdf_temp_E)
     plt.show()
 
+def boxplot_windspeed():
+    WS_A = A.Wind_Speed
+    WS_B = B.Wind_Speed
+    WS_C = C.Wind_Speed
+    WS_D = D.Wind_Speed
+    WS_E = E.Wind_Speed
 
-frequency_polygon()
+    fig = plt.figure(figsize=(28,8))
+    ax1 = fig.add_subplot(151)
+    ax2 = fig.add_subplot(152)
+    ax3 = fig.add_subplot(153)
+    ax4 = fig.add_subplot(154)
+    ax5 = fig.add_subplot(155)
+    ax1.boxplot(WS_A, showmeans=True)
+    ax1.set_ylabel('Wind Speed [m/s]', fontsize=14)
+    ax1.tick_params(labelsize=14)
+    ax2.boxplot(WS_B, showmeans=True)
+    ax2.set_ylabel('Wind Speed [m/s]', fontsize=14)
+    ax2.tick_params(labelsize=14)
+    ax3.boxplot(WS_C, showmeans=True)
+    ax3.set_ylabel('Wind Speed [m/s]', fontsize=14)
+    ax3.tick_params(labelsize=14)
+    ax4.boxplot(WS_D, showmeans=True)
+    ax4.set_ylabel('Wind Speed [m/s]', fontsize=14)
+    ax4.tick_params(labelsize=14)
+    ax5.boxplot(WS_E, showmeans=True)
+    ax5.set_ylabel('Wind Speed [m/s]', fontsize=14)
+    ax5.tick_params(labelsize=14)
+    plt.show()
+
+def boxplot_winddirection():
+    WD_A = A.Direction_True
+    WD_B = B.Direction_True
+    WD_C = C.Direction_True
+    WD_D = D.Direction_True
+    WD_E = E.Direction_True
+
+    fig = plt.figure(figsize=(28,8))
+    ax1 = fig.add_subplot(151)
+    ax2 = fig.add_subplot(152)
+    ax3 = fig.add_subplot(153)
+    ax4 = fig.add_subplot(154)
+    ax5 = fig.add_subplot(155)
+    ax1.boxplot(WD_A, showmeans=True)
+    ax1.set_ylabel('Wind Direction [°]', fontsize=14)
+    ax1.tick_params(labelsize=14)
+    ax2.boxplot(WD_B, showmeans=True)
+    ax2.set_ylabel('Wind Direction [°]', fontsize=14)
+    ax2.tick_params(labelsize=14)
+    ax3.boxplot(WD_C, showmeans=True)
+    ax3.set_ylabel('Wind Direction [°]', fontsize=14)
+    ax3.tick_params(labelsize=14)
+    ax4.boxplot(WD_D, showmeans=True)
+    ax4.set_ylabel('Wind Direction [°]', fontsize=14)
+    ax4.tick_params(labelsize=14)
+    ax5.boxplot(WD_E, showmeans=True)
+    ax5.set_ylabel('Wind Direction [°]', fontsize=14)
+    ax5.tick_params(labelsize=14)
+    plt.show()
+
+def boxplot_temperature():
+    temp_A = A.Temperature
+    temp_B = B.Temperature
+    temp_C = C.Temperature
+    temp_D = D.Temperature
+    temp_E = E.Temperature
+
+    fig = plt.figure(figsize=(28,8))
+    ax1 = fig.add_subplot(151)
+    ax2 = fig.add_subplot(152)
+    ax3 = fig.add_subplot(153)
+    ax4 = fig.add_subplot(154)
+    ax5 = fig.add_subplot(155)
+    ax1.boxplot(temp_A, showmeans=True)
+    ax1.set_ylabel('Temperature [°C]', fontsize=14)
+    ax1.tick_params(labelsize=14)
+    ax2.boxplot(temp_B, showmeans=True)
+    ax2.set_ylabel('Temperature [°C]', fontsize=14)
+    ax2.tick_params(labelsize=14)
+    ax3.boxplot(temp_C, showmeans=True)
+    ax3.set_ylabel('Temperature [°C]', fontsize=14)
+    ax3.tick_params(labelsize=14)
+    ax4.boxplot(temp_D, showmeans=True)
+    ax4.set_ylabel('Temperature [°C]', fontsize=14)
+    ax4.tick_params(labelsize=14)
+    ax5.boxplot(temp_E, showmeans=True)
+    ax5.set_ylabel('Temperature [°C]', fontsize=14)
+    ax5.tick_params(labelsize=14)
+    plt.show()
+
+boxplot_windspeed()
+boxplot_winddirection()
+boxplot_temperature()
 
 
 
+#frequency_polygon()
 #plot_temps_hist_5()
 #plot_temps_hist_50()
