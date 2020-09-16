48142585502ba0dc74ec509a75267baacec4fa67
Michiel de Jong <m.d.dejong-1@student.tudelft.nl>
2020-09-16 09:55:14 +0200

    Added frequency function

--- geo1001_hw01.py
+++ geo1001_hw01.py
@@ -44,8 +44,39 @@ def plot_temps_hist_50():
     ax5.hist(x=temp_E, bins=50, color='hotpink',label='Sensor E' )
     plt.show()
 
+def frequency_polygon():
+    temp_A = A.Temperature
+    temp_B = B.Temperature
+    temp_C = C.Temperature
+    temp_D = D.Temperature
+    temp_E = E.Temperature
+    fig = plt.figure(figsize=(16,8))
+    plt.hist(temp_A, bins=10, alpha=0.5, label='Sensor A', rwidth=0.85)
+    [freq_A, bins]=np.histogram(temp_A, bins = 10) 
+    cdf_temp_A = (freq_A)  
+    plt.plot(bins[:-1], cdf_temp_A)
+    plt.hist(temp_B, bins=10, alpha=0.5, label='Sensor B', rwidth=0.85)
+    [freq_B, bins]=np.histogram(temp_B, bins = 10) 
+    cdf_temp_B = np.cumsum(freq_B)  
+    plt.plot(bins[:-1], cdf_temp_B)
+    plt.hist(temp_C, bins=10, alpha=0.5, label='Sensor C', rwidth=0.85)
+    [freq_C, bins]=np.histogram(temp_C, bins = 10) 
+    cdf_temp_C = np.cumsum(freq_C)  
+    plt.plot(bins[:-1], cdf_temp_C)
+    plt.hist(temp_D, bins=10, alpha=0.5, label='Sensor D', rwidth=0.85)
+    [freq_D, bins]=np.histogram(temp_D, bins = 10) 
+    cdf_temp_D = np.cumsum(freq_D)  
+    plt.plot(bins[:-1], cdf_temp_D)
+    plt.hist(temp_E, bins=10, alpha=0.5, label='Sensor E', rwidth=0.85)
+    [freq_E, bins]=np.histogram(temp_E, bins = 10) 
+    cdf_temp_E = np.cumsum(freq_E)  
+    plt.plot(bins[:-1], cdf_temp_E)
+    plt.show()
+
+
+frequency_polygon()
 
 
 
-plot_temps_hist_5()
-plot_temps_hist_50()
+#plot_temps_hist_5()
+#plot_temps_hist_50()
